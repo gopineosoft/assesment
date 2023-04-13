f1f916c396c250482f249db104de99bdfda4538e
gopi <gopi.chand@neosoftmail.com>
2023-04-13 16:04:52 +0530

    update employee done

--- app/Http/Controllers/API/EmployeeController.php
+++ app/Http/Controllers/API/EmployeeController.php
@@ -114,7 +114,31 @@ class EmployeeController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required|string|max:100',
+            'email'    => 'required|string|email|unique:users,email,'.$id,
+            'password' => 'nullable|min:6|max:15',
+            'department_id' => 'required|numeric',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['message'=>$validator->errors(),'code'=>500]); 
+        }
+
+        try {
+            $user = User::find($id);
+            if(!empty($request['password'])){
+		    	$request['password'] = \Hash::make($request['password']);
+		    }
+            $user->update(['name'=>$request->name,"email"=>$request->email,"password"=>$request->password,"department_id"=>$request->department_id]);
+            $response=[
+                'code'     => 200,
+                'status'=>true,
+                'data'=>'updated'
+            ];
+            return response()->json($response, 200);
+        } catch (Exception $e) {
+            return response()->json(['message'=>'Somethng went wrong','code'=>500]); 
+        }
     }
 
     /**
