3fc1ce0b6acde4b9a2e461a9e0e4cdbd025d1ba1
gopi <gopi.chand@neosoftmail.com>
2023-04-13 11:38:56 +0530

    migration done

--- database/migrations/2023_04_12_103537_create_addresses_table.php
+++ database/migrations/2023_04_12_103537_create_addresses_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->foreignId('department_id');
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('address');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('addresses');
     }
 };
